1e04898a1466035e4f1b5b308cb1f844fd19dbf1
Hadesal <hadealhlo1977@gmail.com>
2024-06-26 15:07:19 +0200

    improved Restaurant section style and renamed functions

--- src/pages/RestaurantSection/RestaurantSection.tsx
+++ src/pages/RestaurantSection/RestaurantSection.tsx
@@ -2,11 +2,13 @@ import { Divider, Stack, Typography } from "@mui/material";
 import styles from "./RestaurantSection.styles";
 import { RestaurantData } from "../../DataTypes/RestaurantObject";
 import BoxComponent from "../../components/DialogComponent/BoxComponent";
-
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import { useSelector } from "react-redux";
 interface RestaurantSectionProps {
   label: string;
 }
 const RestaurantSection = ({ label }: RestaurantSectionProps): JSX.Element => {
+  const restaurantState = useSelector((state) => state.restaurant);
   const restaurants: RestaurantData[] = [
     { id: "1", name: "Restaurant 1", table: [] },
     { id: "2", name: "Restaurant 2", table: [] },
@@ -16,12 +18,15 @@ const RestaurantSection = ({ label }: RestaurantSectionProps): JSX.Element => {
   ];
   const editRestaurant = (restaurant: object): void => {};
   const deleteRestaurant = (restaurant: object) => {};
-  const addRestaurant = () => {};
+  const addRestaurant = () => {
+    deleteRestaurant;
+  };
   return (
     <Stack spacing={3} sx={styles.stack}>
       <Typography variant="h5">{label}</Typography>
       <Divider />
       <BoxComponent
+        CardIcon={RestaurantIcon}
         items={restaurants}
         addFunction={addRestaurant}
         editFunction={editRestaurant}
